f99104f71bea7712ba7dade937fa1a79526dc104
Eray Kısabacak <nefret19981998@hotmail.com>
2020-11-22 22:49:52 +0300

    Login Component Added

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <Navbar/>
+    <router-view></router-view>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue';
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
-}
+    Navbar
+  },
+
+  data(){
+    return {
+      
+    }
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  .theme--light.v-app-bar.v-toolbar.v-sheet {
+    background-color: #fff;
+  }
 </style>
---
+++ src/components/Register.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-content>
+        <v-card width="500" class="mx-auto mt-15">
+            <v-card-title align="center">Register</v-card-title>
+           
+            <v-card-actions>
+                <v-btn color="success" right>Register</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-content>                    
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
