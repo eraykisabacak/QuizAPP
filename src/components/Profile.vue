c798752872c66711804beb9e7fa06274ab147964
Eray Kısabacak <nefret19981998@hotmail.com>
2021-01-14 23:00:27 +0300

    Change Profile Page

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -77,9 +77,58 @@
                 </v-btn>
             </v-card-actions>
 
-            <!-- New Question Add -->
             <div v-if="addQuestionShow == index">
-               <profile-add-question :id="item._id"></profile-add-question>
+                <h4 style="margin-left:15px">Soru Ekleme Alanı</h4>
+                <v-form>
+                <v-row style="align-items: center;justify-content: center;">
+                    <v-col cols="8"
+                            md="8" v-for="(question,q) in inputs.questions" :key="q">
+                            <h3>Yeni Soru</h3>
+                        <v-text-field
+                            v-model="question.questionContent"
+                            prepend-icon="mdi-comment-question"
+                            label="Question"
+                            :rules="[v => !!v || 'Item is required']"
+                            required>
+                        </v-text-field>
+                        <span>
+                            <v-btn depressed color="primary" @click="addQuestion(q)" v-show="q == inputs.questions.length-1">Yeni Soru Ekle</v-btn>
+                            <v-btn depressed color="error" @click="removeQuestion(q)" v-show="q || ( !q && inputs.questions.length > 1)">Soru Sil</v-btn>            
+                        </span>
+                        <v-col cols="12"
+                                md="12" v-for="(correctAnswer,ca) in question.correctAnswers" :key="ca+1000">
+                            <v-text-field 
+                                v-model="correctAnswer.answer"
+                                prepend-icon="mdi-check-outline"
+                                label="CorrectAnswer"
+                                :rules="[v => !!v || 'Item is required']"
+                                required>
+                            </v-text-field>
+                            <span>
+                                <v-btn depressed color="primary" @click="addCorrectAnswer(q)" v-show="ca == question.correctAnswers.length-1">Yeni Doğru Cevap Ekle</v-btn>
+                                <v-btn depressed color="error" @click="removeCorrectAnswer(q,ca)" v-show="ca || ( !ca && question.correctAnswers.length > 1)">Doğru Cevap Sil</v-btn>            
+                            </span>
+                        </v-col>
+                        <v-col cols="12"
+                                md="12" v-for="(incorrectAnswer,ia) in question.incorrectAnswers" :key="ia + 10000">
+                            <v-text-field 
+                                v-model="incorrectAnswer.answer"
+                                prepend-icon="mdi-close-circle-outline"
+                                label="InCorrectAnswer"
+                                :rules="[v => !!v || 'Item is required']"
+                                required>
+                            </v-text-field>
+                            <span>
+                                <v-btn depressed color="primary" @click="addInCorrectAnswer(q)" v-show="ia == question.incorrectAnswers.length-1">Yeni Yanlış Cevap Ekle</v-btn>
+                                <v-btn depressed color="error" @click="removeInCorrectAnswer(q,ia)" v-show="ia || ( !ia && question.incorrectAnswers.length > 1)">Yanlış Cevap Sil</v-btn> 
+                            </span>
+                        </v-col>
+                    </v-col>
+                </v-row>
+                <v-col class="text-right">
+                        <v-btn color="green" class="white--text" :disabled="buttonDisabledNewSaveQuestion" @click="newQuizQuestion(item._id)">Quiz Kaydet ></v-btn>
+                </v-col>
+                </v-form>
             </div>
 
             <!-- Delete Question -->
@@ -264,10 +313,8 @@
 <script>
 import axios from 'axios'
 import { mapGetters } from 'vuex'
-import ProfileAddQuestion from './ProfileAddQuestion.vue'
 
     export default {
-  components: { ProfileAddQuestion },
         data(){
             return {
                 myQuiz:[],
@@ -286,7 +333,19 @@ import ProfileAddQuestion from './ProfileAddQuestion.vue'
                 correctAnswers : [],
                 incorrectAnswers : [],
                 updateAnswerShowTextComp : -1,
-                updateAnswerContent:''
+                updateAnswerContent:'',
+                inputs: {
+                    name: '',
+                    questions: [{
+                        questionContent:'',
+                        correctAnswers : [{
+                            answer:''
+                        }],
+                        incorrectAnswers : [{
+                            answer: ''
+                        }]
+                    }]
+                }
             }
         },
         computed: {
@@ -327,6 +386,31 @@ import ProfileAddQuestion from './ProfileAddQuestion.vue'
                     return true;
                 }
                 return false;
+            },
+            buttonDisabledNewSaveQuestion(){
+                if(this.inputs.questions[0].questionContent == ''){
+                    return true;    
+                }
+                else{
+                    let res = false;
+                    this.inputs.questions.forEach( object => {
+                        if(object.questionContent == ''){
+                            res = true;
+                        }
+                        object.correctAnswers.forEach( correctObject => {
+                            if(correctObject.answer == ''){
+                                res = true;
+                            }
+                        });
+
+                        object.incorrectAnswers.forEach( incorrectObject => {
+                            if(incorrectObject.answer == ''){
+                                res = true;
+                            }
+                        })  
+                    });
+                    return res;
+                }
             }
         },
         created(){
@@ -422,6 +506,7 @@ import ProfileAddQuestion from './ProfileAddQuestion.vue'
                         }
                     })
                     .then(() => {
+                        alert("Soru Silindi");
                         this.getMyQuiz();
                     })
                     .catch(err => console.log(err));
@@ -485,6 +570,7 @@ import ProfileAddQuestion from './ProfileAddQuestion.vue'
                         }
                     })
                     .then(() => {
+                        alert("Soru Güncellendi");
                         this.getMyQuiz();
                     })
                     .catch(err => console.log(err));
@@ -532,11 +618,42 @@ import ProfileAddQuestion from './ProfileAddQuestion.vue'
                         }
                     })
                     .then(() => {
+                        alert("Cevap kayıt edildi");
                         this.getMyQuiz();
                     })
                     .catch(err => console.log(err));
                 }
-            }
+            },
+            newQuizQuestion(id){
+                axios.post('http://localhost:3000/api/question/' + id,
+                {
+                    questionContent: this.inputs.questions[0].questionContent,
+                    correctAnswers:this.inputs.questions[0].correctAnswers,
+                    incorrectAnswers:this.inputs.questions[0].incorrectAnswers
+                }
+                ,{
+                    headers: {
+                        'Authorization': 'Bearer: ' + this.getToken
+                    }
+                })
+                .then( () => {
+                    alert("Question Kayıt Edildi");
+                    this.getMyQuiz();
+                    this.inputs = { 
+                                    name: '',
+                                    questions: [{
+                                        questionContent:'',
+                                        correctAnswers : [{
+                                            answer:''
+                                        }],
+                                        incorrectAnswers : [{
+                                            answer: ''
+                                        }]
+                                    }]
+                                };
+                })
+                .catch(err => console.log(err))
+            },
         },
     }
 </script>
